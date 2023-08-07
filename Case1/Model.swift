598fa3b15d70d8432491b7e65dee24157b545508
Damla KS <damlaksrbst@gmail.com>
2023-08-07 18:13:11 +0300

    first commit

---
+++ Case1/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Case1
+//
+//  Created by Damla KS on 5.08.2023.
+//
+
+import Foundation
---
+++ Case1/SpaceXCell.swift
@@ -0,0 +1,23 @@
+//
+//  SpaceXCell.swift
+//  Case1
+//
+//  Created by Damla KS on 7.08.2023.
+//
+
+import UIKit
+
+class SpaceXCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
